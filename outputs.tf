2522ddb8ae3e42730fdc4bf6e301564bf8c1cc13
AkilBlanchard <akilblanchard@github.com>
2023-10-09 13:33:35 -0500

    Fixed bug where the value of the ouputs was pointing to a managed resource that no longer existed

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.test_env_ec2[*].id
+  value       = module.test_env_module.instance_id
 }
 
 output "instance_public_ip" {
   description = "Public Ip of the EC2 instance(s)"
-  value       = aws_instance.test_env_ec2[*].public_ip
+  value       = module.test_env_module.instance_public_ip
 }
 
 
 output "instance_private_ip" {
   description = "Private Ip of the EC2 instance(s)"
-  value       = aws_instance.test_env_ec2[*].private_ip
+  value       = module.test_env_module.instance_private_ip
 }

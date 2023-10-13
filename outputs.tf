4694ec869baf3844cf7ea4a58fb020524e5c4af7
AkilBlanchard <akilblanchard@github.com>
2023-10-12 23:55:29 -0500

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,4 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = module.test_env_module.instance_id
-}
-
-output "instance_public_ip" {
-  description = "Public Ip of the EC2 instance(s)"
-  value       = module.instance_public_ip
-}
-
-
-output "instance_private_ip" {
-  description = "Private Ip of the EC2 instance(s)"
-  value       = module.test_env.instance_private_ip
+output "state_bucket" {
+  description = "The S3 bucket to store the remote state file."
+  value       = aws_s3_bucket.state
 }

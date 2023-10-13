14c549adff31209420b6c9feb2898a57f1ed653b
AkilBlanchard <akilblanchard@github.com>
2023-10-13 00:08:48 -0500

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "instance_id" {
   value       = module.test_env_module.instance_id
 }
 
-output "instance_public_ip" {
-  description = "Public Ip of the EC2 instance(s)"
-  value       = module.test_env_module.public_private_ip
-}
-
 
 output "instance_private_ip" {
   description = "Private Ip of the EC2 instance(s)"

e651354cdde60a569b1857b64f7a2b27b2f566b8
AkilBlanchard <akilblanchard@github.com>
2023-10-13 23:09:34 -0500

    Corrected output values and tested to ensure desired output was produced

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = module.test_env_module.instance_id
-}
-
-
 output "instance_private_ip" {
   description = "Private Ip of the EC2 instance(s)"
-  value       = module.test_env_module.instance_private_ip
+  value       = aws_instance.test_env_ec2[*].private_ip
+
 }
 
 output "instance_public_ip" {
   description = "Public Ip of the EC2 instance(s)"
-  value       = module.test_env_module.instance_public_ip
+  value       = aws_instance.test_env_ec2[*].public_ip
 }
+
+output "kms_key" {
+  description = "The KMS customer master key to encrypt state buckets."
+  value       = aws_kms_key.state.id
+}
+
